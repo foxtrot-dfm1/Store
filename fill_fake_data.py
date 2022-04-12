7d69313a7e7e43b16a0b67c57b98a37a23d68b48
Roman <72063601+foxtrot-dfm1@users.noreply.github.com>
2022-04-12 15:40:27 +0200

    Update fill_fake_data.py

--- fill_fake_data.py
+++ fill_fake_data.py
@@ -2,6 +2,6 @@ from app.models import Product
 from app import db, create_app
 
 with create_app().app_context():
-    for i in range(0, 10):
+    for i in range(1, 10):
             db.session.add(Product(name="name" + str(i), price=i, description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", available_quantity=i))
             db.session.commit()
