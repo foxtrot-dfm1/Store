ea1b2dd3ef019bed075188966201f7af1e3b1c87
foxtrot-dfm1 <kysilroman99@gmail.com>
2022-04-14 14:35:08 +0200

    Deploy

--- app/config.py
+++ app/config.py
@@ -1,5 +1,26 @@
-class Configuration(object):
+import os
+class BaseConfig(object):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///../DB.sqlite3'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'change on deploy'
+
+class HerokuConfig(BaseConfig):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+def get_config():
+    if os.environ.get('PROVIDER') == 'heroku':
+        HerokuConfig.SQLALCHEMY_DATABASE_URI = \
+            HerokuConfig.SQLALCHEMY_DATABASE_URI.replace(
+                    "postgres://", "postgresql://", 1
+                )
+
+        return HerokuConfig
+    
+    else:
+
+        return BaseConfig
+
